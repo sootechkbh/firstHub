7de91fd30574e952bc23c3aef920f99567447dc5
kbh <kbh@DESKTOP-1G1PDJ3>
2020-05-21 17:52:45 +0900

    asdasd

--- firstHub/src/main/java/firstHub/Controller.java
+++ firstHub/src/main/java/firstHub/Controller.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class Controller {
 
 	public static void main(String[] args) {
-
-	System.out.println("new test3333 jhj!! ");
+		
+		System.out.println("new test3d12312312312 kbh!! ");
 	}
 
+	
 	
 }
