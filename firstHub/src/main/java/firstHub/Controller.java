c676e22bb5dc2754a8d532f9809029e61fa8c69c
kbh <kbh@DESKTOP-1G1PDJ3>
2020-05-21 17:55:12 +0900

    fdgdfgdfg

--- firstHub/src/main/java/firstHub/Controller.java
+++ firstHub/src/main/java/firstHub/Controller.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Controller {
 
 	public static void main(String[] args) {
-		
-		System.out.println("new test3d12312312312 kbh!! ");
+		System.out.println("real  kbh ");
+		System.out.println("real jhj");
 	}
 
 	
