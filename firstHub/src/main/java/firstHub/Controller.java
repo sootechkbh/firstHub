1c88b0658cc5891bc79bad10c79d8a26a7689ca8
kbh <kbh@DESKTOP-1G1PDJ3>
2020-05-21 16:10:40 +0900

    ss

--- firstHub/src/main/java/firstHub/Controller.java
+++ firstHub/src/main/java/firstHub/Controller.java
@@ -11,5 +11,25 @@ public class Controller {
 		System.out.println("third Branch");	
 		System.out.println("third Branch");	
 	}
+	
+	public void a() {
+		System.out.println("second Branch a");
+	}
+	public void b() {
+		System.out.println("second Branch b");	
+	}
+	public void c() {
+		System.out.println("second Branch c");
+	}
+	public void d() {
+		System.out.println("second Branch d");
+	}
+	public void e() {
+		System.out.println("second Branch e");	
+	}
+	public void f() {
+		System.out.println("second Branch f");
+	}
+
 
 }
