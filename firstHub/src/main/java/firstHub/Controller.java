addc132f9d82f329861b17277dea6624374c55ae
kbh <kbh@DESKTOP-1G1PDJ3>
2020-05-21 14:55:04 +0900

    3

--- firstHub/src/main/java/firstHub/Controller.java
+++ firstHub/src/main/java/firstHub/Controller.java
@@ -1,9 +1,14 @@
 package firstHub;
 
+import java.util.Scanner;
+
 public class Controller {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("1");
 
 	}
 
