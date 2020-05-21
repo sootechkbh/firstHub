cee05d6e7dbb2da2ef387334dc8774855ee5818d
kbh <kbh@DESKTOP-1G1PDJ3>
2020-05-21 17:45:18 +0900

    asdasdasd

--- firstHub/src/main/java/firstHub/Controller.java
+++ firstHub/src/main/java/firstHub/Controller.java
@@ -7,7 +7,6 @@ public class Controller {
 	public static void main(String[] args) {
 		
 		System.out.println("new test3d12312312312 kbh!! ");
-		System.out.println("new test312312312312 kbh!! ");	
 	}
 
 	
