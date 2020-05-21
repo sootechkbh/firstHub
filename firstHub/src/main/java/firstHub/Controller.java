2225b442018abb3ab3f53e16a54e471a805ac7b8
kbh <kbh@DESKTOP-1G1PDJ3>
2020-05-21 16:53:36 +0900

    dddd

--- firstHub/src/main/java/firstHub/Controller.java
+++ firstHub/src/main/java/firstHub/Controller.java
@@ -9,4 +9,23 @@ public class Controller {
 	System.out.println("master Branch test");
 	}
 	
+	public void a() {
+		System.out.println("master a");
+	}
+	public void b() {
+		System.out.println("master b");
+	}
+	public void c() {
+		System.out.println("master c");
+	}
+	public void d() {
+		System.out.println("master d");
+	}
+	public void e() {
+		System.out.println("master e");
+	}
+	public void f() {
+		System.out.println("master test");
+	}
+	
 }
