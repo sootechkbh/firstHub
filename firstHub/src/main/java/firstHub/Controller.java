23b3fba8106514b5c2f5a805adf0e5f4b9479e8a
kbh <kbh@DESKTOP-1G1PDJ3>
2020-05-21 17:11:18 +0900

    second test!

--- firstHub/src/main/java/firstHub/Controller.java
+++ firstHub/src/main/java/firstHub/Controller.java
@@ -5,30 +5,10 @@ import java.util.Scanner;
 public class Controller {
 
 	public static void main(String[] args) {
-
-		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("second Branch ");	
-		System.out.println("second Branch ");
+		System.out.println("second test! ");	
 	}
-	public void a() {
-		System.out.println("second a");
-	}
-	public void b() {
-		System.out.println("second b");
-	}
-	public void c() {
-		System.out.println("second c");
-	}
-	public void d() {
-		System.out.println("second d");
-	}
-	public void e() {
-		System.out.println("second e");
-	}
-	public void f() {
-		System.out.println("second test");
-	}
+
 	
 	
 }
