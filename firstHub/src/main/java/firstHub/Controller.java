702473b58e319a64ff3e8c829cc57e4823566a03
kbh <kbh@DESKTOP-1G1PDJ3>
2020-05-21 17:16:42 +0900

    .....

--- firstHub/src/main/java/firstHub/Controller.java
+++ firstHub/src/main/java/firstHub/Controller.java
@@ -6,7 +6,7 @@ public class Controller {
 
 	public static void main(String[] args) {
 
-	System.out.println("master Branch test!!!!!!");
+	System.out.println("master Branch test!!!!!!jhj!!!");
 	}
 
 	
