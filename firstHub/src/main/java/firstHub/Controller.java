e4d51a53613db83a942883d0b3388b199b27813c
kbh <kbh@DESKTOP-1G1PDJ3>
2020-05-21 15:01:29 +0900

    5

--- firstHub/src/main/java/firstHub/Controller.java
+++ firstHub/src/main/java/firstHub/Controller.java
@@ -7,27 +7,26 @@ public class Controller {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
-		
 		System.out.println("1");
 		System.out.println("1");
+		System.out.println("1");System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");System.out.println("1");
 		System.out.println("1");
 		System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");
+
 
 	}
 
