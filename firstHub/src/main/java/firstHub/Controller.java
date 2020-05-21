570ae5c9da74f19a58c7d1fc472459ac66877a05
kbh <kbh@DESKTOP-1G1PDJ3>
2020-05-21 15:20:45 +0900

    master Branch Commit

--- firstHub/src/main/java/firstHub/Controller.java
+++ firstHub/src/main/java/firstHub/Controller.java
@@ -7,25 +7,7 @@ public class Controller {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
-		System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");System.out.println("1");
-		System.out.println("1");
-		System.out.println("1");
+		System.out.println("master Branch");
 
 
 	}
